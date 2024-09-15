fc4e5445d785a6ddd6c819aabf3c1620f059038c
Marcelo Rezin <marcelo.rezin@gmail.com>
2024-09-15 12:29:35 -0300

    Testes para default warning operation

--- cmd/go-log/cmd_default_operation_test.go
+++ cmd/go-log/cmd_default_operation_test.go
@@ -8,6 +8,9 @@ import (
 const EXPECTED_INFO_PREFIX = "INFO: "
 const EXPECTED_INFO_FLAG = log.Ldate | log.Ltime
 
+const EXPECTED_WARNING_PREFIX = "WARNING: "
+const EXPECTED_WARNING_FLAG = log.Ldate | log.Ltime | log.Lshortfile
+
 func TestGetDefaultInfo(t *testing.T) {
 	op := GetDefaultInfo()
 
@@ -20,6 +23,22 @@ func TestGetDefaultInfo(t *testing.T) {
 	flagActual := op.Flag
 	if flagActual != EXPECTED_INFO_FLAG {
 		t.Log("flag should be", EXPECTED_INFO_FLAG, "but got", flagActual)
+		t.Fail()
+	}
+}
+
+func TestGetDefaultWarning(t *testing.T) {
+	op := GetDefaultWarning()
+
+	prefixActual := op.Prefix
+	if prefixActual != EXPECTED_WARNING_PREFIX {
+		t.Log("prefix should be", EXPECTED_WARNING_PREFIX, "but got", prefixActual)
+		t.Fail()
+	}
+
+	flagActual := op.Flag
+	if flagActual != EXPECTED_WARNING_FLAG {
+		t.Log("flag should be", EXPECTED_WARNING_FLAG, "but got", flagActual)
 		t.Fail()
 	}
 }
